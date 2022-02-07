08b0a28b96c54e080d32e8c149e68d464507dfa5
CheonGyeHwan <cgh5531@naver.com>
2022-02-07 10:50:33 +0900

    wiring #1

---
+++ container/src/main/java/com/poscoict/container/user/User1.java
@@ -0,0 +1,12 @@
+package com.poscoict.container.user;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class User1 {
+	
+	public String getName() {
+		return "천계환";
+	}
+
+}
