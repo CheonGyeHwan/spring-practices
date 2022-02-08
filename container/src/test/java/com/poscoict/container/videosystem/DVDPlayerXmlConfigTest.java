8c56e41b16930cb9f91fc6d140a556fcb46cbe48
CheonGyeHwan <cgh5531@naver.com>
2022-02-08 12:40:16 +0900

    explicit - xml#2

--- container/src/test/java/com/poscoict/container/videosystem/DVDPlayerXmlConfigTest.java
+++ container/src/test/java/com/poscoict/container/videosystem/DVDPlayerXmlConfigTest.java
@@ -27,6 +27,33 @@ public class DVDPlayerXmlConfigTest {
 	@Autowired
 	private DigitalVideoDisc dvd2;
 	
+	@Qualifier("avengersInfinityWar")
+	@Autowired
+	private DigitalVideoDisc dvd3;
+	
+	@Qualifier("avengersEndGame")
+	@Autowired
+	private DigitalVideoDisc dvd4;
+	
+	@Qualifier("avengersAgeOfUltron")
+	@Autowired
+	private DigitalVideoDisc dvd5;
+	
+	@Qualifier("avengersCaptainAmerica")
+	@Autowired
+	private DigitalVideoDisc dvd6;
+	
+	@Qualifier("avengersDirectorEdition")
+	@Autowired
+	private DigitalVideoDisc dvd7;
+	
+	@Qualifier("avengersExpansionEdition1")
+	@Autowired
+	private DigitalVideoDisc dvd8;
+	
+	@Autowired
+	private DVDPack dvdPack;
+	
 	@Ignore
 	@Test
 	public void testDvd1() {
@@ -36,6 +63,41 @@ public class DVDPlayerXmlConfigTest {
 	@Test
 	public void testDvd2() {
 		assertNotNull(dvd2);
+	}
+	
+	@Test
+	public void testDvd3() {
+		assertNotNull(dvd3);
+	}
+	
+	@Test
+	public void testDvd4() {
+		assertNotNull(dvd4);
+	}
+	
+	@Test
+	public void testDvd5() {
+		assertNotNull(dvd5);
+	}
+	
+	@Test
+	public void testDvd6() {
+		assertNotNull(dvd6);
+	}
+	
+	@Test
+	public void testDvd7() {
+		assertNotNull(dvd7);
+	}
+	
+	@Test
+	public void testDvd8() {
+		assertNotNull(dvd8);
+	}
+	
+	@Test
+	public void testDvdPack() {
+		assertNotNull(dvdPack);
 	}
 	
 }
