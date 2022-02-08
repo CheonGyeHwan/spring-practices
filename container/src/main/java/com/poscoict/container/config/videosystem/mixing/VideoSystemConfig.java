45bdd2459b105964326525e9b3ca60fb9669dfe4
CheonGyeHwan <cgh5531@naver.com>
2022-02-08 15:43:46 +0900

    mixing config

---
+++ container/src/main/java/com/poscoict/container/config/videosystem/mixing/VideoSystemConfig.java
@@ -0,0 +1,19 @@
+package com.poscoict.container.config.videosystem.mixing;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * 
+ * <---------- JavaConfig1(DVDConfig) + JavaConfig2(DVDPlayerConfig)
+ *    import
+ *    
+ *    JavaConfig1 + JavaConfig2
+ *    
+ */
+
+@Import({DVDConfig.class, DVDPlayerConfig.class})
+@Configuration
+public class VideoSystemConfig {
+
+}
